a8f41cd7905dddd3349f03d8de8619294361d50a
achref <achrefbs>
2020-09-19 03:08:49 +0100

    add rules

--- Python/menu.py
+++ Python/menu.py
@@ -8,7 +8,7 @@ from main import start_game
 
 class Game():
     pygame.init()
-    surface = pygame.display.set_mode((400, 550))
+    surface = pygame.display.set_mode((450, 600))
 
     def set_difficulty(value, difficulty):
         if value == 1:
@@ -22,13 +22,11 @@ class Game():
 
     def Play_Mode(mode, value):
         pass
-
-    menu = pygame_menu.Menu(550, 400, 'MasterMind Game',
-                        theme=pygame_menu.themes.THEME_SOLARIZED)
-
-    menu.add_text_input('Enter Your Name :', default='Master')
-    menu.add_selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
-    menu.add_selector('Play Mode :', [('Single Player', 1), ('Two Players', 2)], onchange=Play_Mode)
+    pygame.display.set_caption("Mastermind")
+    menu = pygame_menu.Menu(600, 450, 'MasterMind',
+                        theme=pygame_menu.themes.THEME_DARK)
+    menu.add_selector('Difficulty : ', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+    menu.add_selector('Play Mode : ', [('Single Player', 1), ('Two Players', 2)], onchange=Play_Mode)
     menu.add_button('Play', start_the_game)
     menu.add_button('Quit', pygame_menu.events.EXIT)
     menu.mainloop(surface)
