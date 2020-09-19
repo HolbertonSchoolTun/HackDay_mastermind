795056bc99930e5af625004dd7ee17fc30752678
achref <achrefbs>
2020-09-19 01:13:45 +0100

    up

--- Python/main.py
+++ Python/main.py
@@ -11,7 +11,6 @@ def start_game():
     LEFT = 1
     RIGHT = 3
     COLOR = 0
-    # Define some colors
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
     GREEN = (0, 204, 0)
@@ -24,49 +23,37 @@ def start_game():
     rows = 12
     columns = 6
     turn = 11
-    # This sets the WIDTH and HEIGHT of each grid location
     WIDTH = 40
     HEIGHT = 40
     
-    # This sets the margin between each cell
     MARGIN = 5
     
-    # Create a 2 dimensional array. A two dimensional
-    # array is simply a list of lists.
+
     grid = []
     for row in range(rows):
-        # Add an empty array that will hold each cell
-        # in this row
+
         grid.append([])
         for column in range(columns):
-            grid[row].append(0)  # Append a cell
+            grid[row].append(0)
     
-    # Initialize pygame
     pygame.init()
     
-    # Set the HEIGHT and WIDTH of the screen
     WINDOW_SIZE = [300, 600]
     screen = pygame.display.set_mode(WINDOW_SIZE)
     
-    # Set title of screen
     pygame.display.set_caption("Mastermind")
     
-    # Loop until the user clicks the close button.
     done = False
     
-    # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
     m = SinglePlayer()
    
-    # -------- Main Program Loop -----------
     while not done:
-        for event in pygame.event.get():  # User did something
-            if event.type == pygame.QUIT:  # If user clicked close
-                done = True  # Flag that we are done so we exit this loop
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT:  
+                done = True 
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:
-                # User clicks the mouse. Get the position
                 pos = pygame.mouse.get_pos()
-                # Change the x/y screen coordinates to grid coordinates        
                 column = pos[0] // (WIDTH + MARGIN)
                 row = pos[1] // (HEIGHT + MARGIN)
                 if turn == row and column < 4:
@@ -78,32 +65,22 @@ def start_game():
                     turn -= 1
                     m.Player_Input(result)
                     m.compare()
-                    #print(result)
-                    #print(m.playerinput)
                     print(m.objective)
-                #print(COLOR)
                 print("Click ", pos, "Grid coordinates: ", row, column)
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == RIGHT:
-                # User clicks the mouse. Get the position
                 pos = pygame.mouse.get_pos()
-                # Change the x/y screen coordinates to grid coordinates
                 column = pos[0] // (WIDTH + MARGIN)
                 row = pos[1] // (HEIGHT + MARGIN)
-                # Set that location to one
                 if turn == row:
 
                     COLOR -= 1
                     COLOR = COLOR % 7
                     grid[row][column] = COLOR
                     result[column] = COLOR
-                #print(COLOR)
                 print("Click ", pos, "Grid coordinates: ", row, column)
 
     
-        # Set the screen background
         screen.fill(BLACK)
-    
-        # Draw the grid
         for row in range(rows):
             for column in range(columns):
                 if column < 4:
@@ -129,13 +106,7 @@ def start_game():
                         pygame.draw.circle(screen, GREEN, (a, s+ 10), 5, 0)
                         a += 10
             s -= 45
-        #print(b)
-        # Limit to 60 frames per second
         clock.tick(120)
-
-        # Go ahead and update the screen with what we've drawn.
         pygame.display.flip()
-    
-    # Be IDLE friendly. If you forget this line, the program will 'hang'
-    # on exit.
+
     pygame.quit()
